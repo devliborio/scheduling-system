79de652ded8b91983dbadffd696b54e5d3369cca
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-04-19 18:05:34 -0300

    Enviando setup do projeto

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://127.0.0.1:27017/scheduling')
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Test!");
+})
+
+app.listen(9090, (req, res) => {
+    console.log("Servidor Rodando!")
+})
